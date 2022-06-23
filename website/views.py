04e50aa5ae2e1bec190c3ae25b73e6734137c5bc
Ubadah Saleh <99445739+SquidQuid@users.noreply.github.com>
2022-06-23 16:40:06 -0500

    Add files via upload

---
+++ website/views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@views.route('/content')
+def content():
+    return render_template("content.html")
